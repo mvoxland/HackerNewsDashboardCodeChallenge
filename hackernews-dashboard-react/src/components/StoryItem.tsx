9ac9693bb93b4eed7c2a2d577fabaec859532437
mvox <108884442+mvoxland@users.noreply.github.com>
2025-11-05 22:30:51 -0600

    add more info to story item

--- hackernews-dashboard-react/src/components/StoryItem.tsx
+++ hackernews-dashboard-react/src/components/StoryItem.tsx
@@ -9,9 +9,16 @@ const StoryItem: React.FC<StoryItemProps> = ( {story} ) => {
     return (
         <div className="story-item">
             <h3>{story.title}</h3>
+            <a href={story.url} target="_blank" rel="noopener noreferrer" className='wrap-text'>{story.url}</a>
             <p>By: {story.by}</p>
-            <p>Points: {story.score}</p>
-            <a href={story.url} target="_blank" rel="noopener noreferrer">Read more</a>
+            <p>Score: {story.score}</p>
+            <p>Comments: {story.kids?.length} ({story.descendants})</p>
+            <p>Posted: {story.time ? new Date(story.time * 1000).toLocaleString(undefined, { dateStyle: 'short', timeStyle: 'short'}) : 'N/A'}</p>
+
+            <hr />
+
+            <p>User Comments: {story.userComments?.length}</p>
+            <p>Your Rating: {story.userRating?.ratingStars ? story.userRating?.ratingStars + " stars" : ""}</p>
         </div>
     );
 };
